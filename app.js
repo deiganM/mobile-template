d913f0edcb93dd662434ea678fc0b79fcc867a36
Deigan Millikin <deiganmillikin@gmail.com>
2020-02-27 15:04:25 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,12 @@
+const slidingNav = () => {
+  const burger = document.querySelector('.burger')
+  const nav = document.querySelector('.nav-links')
+  
+
+  burger.addEventListener('click', () => {
+    nav.classList.toggle('nav-active')
+    burger.classList.toggle('close')
+  })
+}
+
+slidingNav()
